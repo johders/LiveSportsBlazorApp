57b96f858d381a3098292f68e9d98be31945aa45
jd-HW <johannes.dereuddre@student.howest.be>
2024-12-23 11:44:36 +0100

    Create reported dashboard and manual input components

--- Pin.LiveSports.Core/Services/Interfaces/IReportService.cs
+++ Pin.LiveSports.Core/Services/Interfaces/IReportService.cs
@@ -4,10 +4,8 @@ namespace Pin.LiveSports.Core.Services.Interfaces
 {
     public interface IReportService
     {
-        List<string> GetMessages();
         Matchup GetMatchup();
         List<ReportEventLog> GetEventLogs();
-        void SetAnnouncement(string message);
         void SetMatchup(Matchup matchup);
         void SetEventLog(ReportEventLog eventLog);
     }
--- Pin.LiveSports.Core/Services/ReportService.cs
+++ Pin.LiveSports.Core/Services/ReportService.cs
@@ -5,29 +5,16 @@ namespace Pin.LiveSports.Core.Services
 {
     public class ReportService : IReportService
     {
-        private readonly List<string> _messages = new();
         private Matchup _matchup = new();
-
-        public List<string> GetMessages()
-        {
-            return _messages;
-        }
 
         public List<ReportEventLog> GetEventLogs()
         {
             return _matchup.EventLogs;
         }
-
         public Matchup GetMatchup()
         {
             return _matchup;
         }
-
-        public void SetAnnouncement(string message)
-        {
-            _messages.Add(message);
-        }
-
         public void SetMatchup(Matchup matchup)
         {
             _matchup = matchup;
